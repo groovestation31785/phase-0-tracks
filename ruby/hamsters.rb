8cac886329e8b436453d936127388be30a5795f3
Allyse Groover <angroover@gmail.com>
2016-10-26 10:32:13 -0400

    Create hamster questionnaire

---
+++ ruby/hamsters.rb
@@ -0,0 +1,27 @@
+puts "What is the hamster's name?"
+hamnm = gets.chomp
+put "So the hamster's name is #{hamnm}."
+
+puts "How deeply do you sleep, scale of one to ten?"
+vollev = gets.chomp.to_i
+puts "Alright. I will put down #{vollev}."
+
+puts "What color of hamster do you want?"
+hamcol = gets.chomp
+puts "A #{hamcol} hamster"
+
+puts "Is the hamster a good candidate for adoption?"
+adopt = gets.chomp
+puts "So the hamster is a #{adopt} candidate."
+
+puts "How old is the hamster?"
+age = gets.chomp
+
+
+if age = ""
+	age = nil
+	puts "You aren't sure of the age?"
+elsif age = age.to_i
+	puts "OK. The hamster is #{age} years old."
+	age = age.to_i
+end
