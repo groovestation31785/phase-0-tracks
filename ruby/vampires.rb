4db090b74cca9852baf162ed2d410363f37ba66a
Allyse Groover <angroover@gmail.com>
2016-10-26 14:27:56 -0400

    Add questions for vampires

---
+++ ruby/vampires.rb
@@ -0,0 +1,13 @@
+puts "What's your name?"
+name = gets.chomp
+
+puts "How old are you? What year were you born?"
+age = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+insurance = gets.chomp
+
+
