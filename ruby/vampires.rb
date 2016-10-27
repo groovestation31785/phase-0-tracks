e8bd5db50d0108158f6e2b183ecc89080ea741b6
Allyse Groover <angroover@gmail.com>
2016-10-27 12:52:16 -0400

    Creat loop for allergies

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,7 +1,7 @@
 puts "How many employees will be processed?"
 num_empl = gets.chomp.to_i
 
-until num_empl == 0
+until num_empl <= 0 do
 
 	puts "What's your name?"
 	name = gets.chomp
@@ -21,17 +21,33 @@ until num_empl == 0
 	insurance = gets.chomp
 
 
-	if name == "Drake Cula" || name =="Tu Fang"
-		puts "Definitely a vampire."
-	elsif (accurate_age != given_age) && (bread == "n") && (insurance == "n")
-		puts "Almost certainly a vampire"
-	elsif (accurate_age != given_age) && (bread == "n" || insurance == "n")
-		puts "Probably a vampire"
-	elsif (accurate_age == given_age) && (bread == "y" || insurance == "y")
-		puts "Probably not a vampire"
-	else
-		puts "Results inconclusive."
-	end
+	puts "List any allergies you have. Type 'done' when you are finished or if you don't have any allergies."
+	allergies = gets.chomp.downcase
+
+		until allergies == "sunshine" || allergies == "done"
+			puts "Anything else?"
+			allergies = gets.chomp.downcase
+		end
+	
+
+
+		if name == "Drake Cula" || name =="Tu Fang"
+			puts "Definitely a vampire."
+		elsif allergies == "sunshine"
+			puts "Probably a vampire"
+		elsif (accurate_age != given_age) && (bread == "n") && (insurance == "n")
+			puts "Almost certainly a vampire"
+		elsif (accurate_age != given_age) && (bread == "n" || insurance == "n") 
+			puts "Probably a vampire"
+		elsif (accurate_age == given_age) && (bread == "y" || insurance == "y")
+			puts "Probably not a vampire"
+		else
+			puts "Results inconclusive."
+		end
+
 
 	num_empl -= 1	
 end
+
+
+puts "Actually, nevermind!!! What do these questions have to do with anthing? Let's all be friends. :D" 
