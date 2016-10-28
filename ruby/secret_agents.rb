cd6e2a6924e86f7a42d95909ada01ec60131c497
Allyse Groover <angroover@gmail.com>
2016-10-27 23:42:37 -0400

    Update decrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -14,31 +14,61 @@ def encrypt(str)
 
 
 	while index < str_leng
+		
 		print str[index].next
+
+		if str[index] == " "
+		end
+		
 	index += 1
 
 	end
 end
 
+print encrypt("hello")
 
 
 def decrypt(str)
-	
-	index = -1
+	alpha = "abcdefghijklmnopqrstuvwxyz"
+	index = 0
 	str_leng = str.length
 
+	
 
-	while index < str_leng
-		print str[index].next
-	index += 1
 
-	end
 end
 
-puts "Type a word"
-string = gets.chomp
-
-print encrypt(string)
 
 
-print decrypt(string)
+
+
+
+=begin we did a loop just like the encrypt one… but the 
+conditional was a bit different, since we don’t have a 
+.previous method to change the character for its previous one
+
+angroover [8:54 PM]  
+Did you use the alphabet to find the position of the letter, 
+so that the number would be the thing moving backward?
+
+elaine [8:54 PM]  
+oh, I can show you our pseudocode, maybe just this will help
+
+[8:54]  
+
+The decrypt method needs to accept a string;
+We need a loop to go through each character of the string;
+We need to find the index of each character of the alphabet:
+if character is not a space character,
+    we change the character to the alphabet-string-character
+     at index minus one.
+
+
+[8:55]  
+yeah, we used the alphabet variable to find the index of the 
+letters
+
+[8:56]  
+and we subtracted one to this index
+
+=end
