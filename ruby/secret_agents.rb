fcfb238b4ee96d55d040f126d9d62008e466eb05
Allyse Groover <angroover@gmail.com>
2016-10-28 17:35:24 -0400

    Update code for decrypt method/interface

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,74 +1,94 @@
-# Ask user for the password
-# Retrieve passwork 
-# Cycle through each letter of the password
-	# Until the index number reaches nil:
-		# Use the next method to get the next letter
+=begin
+ENCRYPT PSEUDOCODE
+-Ask the user to type a password
+	-Cycle through each letter of the password
+	Until the index number reaches nil:
+		Use the next method to get the next letter
 
-
-
-
+=end
 
 def encrypt(str)
-	index = 0
-	str_leng = str.length
+ password = str
+ index = 0
 
-
-	while index < str_leng
-		
-		print str[index].next
-
-		if str[index] == " "
-		end
-		
-	index += 1
-
-	end
+ while index < password.length do
+     if password[index] == "z"    #if the index character is "z" it will
+       password[index] = "a"      #change that character to "a" to avoid edge cases
+     elsif (password[index] =~ /\s+/) #detects if there is a space in the string
+       password[index] = " "      #prints the space character as is, unchanged.
+     else
+       password[index] = password[index].next!  #prints the character at that index number
+     end
+   index += 1              # increases the index number
+ end
+ puts password
 end
 
-print encrypt("hello")
+=begin
+DECRYPT PSEUDOCODE
+Ask the user to type a password:
+	-Cycle through each character to find its corresponding index number within the alphabet
+	-Substract that index's number by one
+	-Retrieve the new index number
+	-Match the letter with the corresponding index
+	-For every space character:
+		-Do not move the space to the previous character.
+		-Use the space character
+	-Continue through all the letters until you reach the end of the given string
+
+=end
 
 
 def decrypt(str)
-	alpha = "abcdefghijklmnopqrstuvwxyz"
-	index = 0
-	str_leng = str.length
+ password = str
+ something = 0
+ alpha_index = 0
+ alpha = "abcdefghijklmnopqrstuvwxyz"
 
-	
-
-
+ while something < password.length do
+     if (password[something] =~ /\s/)
+       str[something] = " "
+     else
+       alpha_index = alpha.index(password[something])
+       alpha_index -= 1
+       password[something] = alpha[alpha_index]
+     end
+   something += 1
+ end
+ puts password
 end
 
+# Driver code
+# decrypt(encrypt("swordfish"))      # Encrypt runs the string and returns a new string
+                                  # Decrypt uses the encrypted string as an argument
+                                  # and decrypts to a new string
 
 
+=begin
+ Ask the user if they want their password encrypted or decrpyted.
+   If they answer "encrypted" ask for password and run encrypt method
+   Or they answer "decrypted" ask for password and run decrypt method
+   Otherwise tell them invalid repsonse and ask first question again
 
-
-
-=begin we did a loop just like the encrypt one… but the 
-conditional was a bit different, since we don’t have a 
-.previous method to change the character for its previous one
-
-angroover [8:54 PM]  
-Did you use the alphabet to find the position of the letter, 
-so that the number would be the thing moving backward?
-
-elaine [8:54 PM]  
-oh, I can show you our pseudocode, maybe just this will help
-
-[8:54]  
-
-The decrypt method needs to accept a string;
-We need a loop to go through each character of the string;
-We need to find the index of each character of the alphabet:
-if character is not a space character,
-    we change the character to the alphabet-string-character
-     at index minus one.
-
-
-[8:55]  
-yeah, we used the alphabet variable to find the index of the 
-letters
-
-[8:56]  
-and we subtracted one to this index
-
+   Print the password
 =end
+
+puts "Would you like to decrypt or encrypt your password"
+response = gets.chomp
+
+until response == "encrypt" || response == "decrypt"
+ puts "Try that again."
+ response = gets.chomp
+end 
+
+if response == "decrypt"
+     puts "What is the password?"
+     password = gets.chomp
+     decrypt(password)
+elsif response == "encrypt"
+     puts "What is the password"
+     password = gets.chomp
+     encrypt(password)
+   else
+     puts "Invalid response"
+end
