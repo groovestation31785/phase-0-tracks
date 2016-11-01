ce8d3c09b22af6dad1eb96b0ee26249dc7c645ec
Allyse Groover <angroover@gmail.com>
2016-11-01 19:25:33 -0400

    Find case, ord, and prepend methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,8 +4,10 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".swapcase
+ # => “InVeStIgAtIoN”
+
+"SpOoKy HaLLowEEn!".swapcase
 
 # "zom".<???>
 # => “zoom”
@@ -13,14 +15,19 @@
 # "enhance".<???>
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
+
+"I'm not shouting!".upcase
+
 
 # "the usual".<???>
 #=> "the usual suspects"
 
-# " suspects".<???>
+" suspects".prepend("the usual")
 # => "the usual suspects"
+
+" by accident.".prepend("I found this")
 
 # "The case of the disappearing last letter".<???>
 # => "The case of the disappearing last lette"
@@ -31,9 +38,12 @@
 # "Elementary,    my   dear        Watson!".<???>
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+
+"h".ord
+"i".ord
 
 # "How many times does the letter 'a' appear in this string?".<???>
 # => 4
