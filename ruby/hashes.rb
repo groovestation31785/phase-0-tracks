e0bf7231a9d236a6fe74878ffd53cd1e92f8895a
Allyse Groover <angroover@gmail.com>
2016-11-02 09:34:47 -0400

    Add prompt questions for decor info

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -18,7 +18,27 @@ Show the user the updated response
 =end
 
 
+client_details {}
+
+puts "What is your name?"
+name = gets.chomp
+
+puts "How many children do you have?"
+children = get.chomp.to_i
+
+puts "Describe your preferred decor theme."
+decor_theme = gets.chomp
+
+puts "You have a minimalist style: true or false?"
+minimalist = gets.chomp
+
+puts "How many rooms do you have in your house?"
+num_rooms = gets.chomp.to_i
+
+puts "What are some of your favorite colors?"
+fav_colors = gets.chomp
 
 
 
+p client_details[:name]
 
