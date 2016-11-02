c2807eeacc1a032378bf4f4e992820963615f23e
Allyse Groover <angroover@gmail.com>
2016-11-02 09:10:52 -0400

    Add pseudocode to hash file

---
+++ ruby/hashes.rb
@@ -0,0 +1,24 @@
+=begin
+Ask the user for the following information:
+	-Name
+	-Number of children
+	-Decor theme
+	-Minimalist style (t/f)
+	-Number of rooms in their house
+	-Favorite colors
+	If their response is a number:
+		-Convert string answer to integer
+Show the user their responses in Hash form
+	If the user thinks there is a mistake:
+		-Ask for an update:
+			-Allow them to type the change
+	-Otherwise:
+		-They should type "done" if there are no changes
+Show the user the updated response
+=end
+
+
+
+
+
+
