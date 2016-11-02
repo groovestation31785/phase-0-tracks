a3710f530738dc11072430175958663524e9ebce
Allyse Groover <angroover@gmail.com>
2016-11-02 14:25:50 -0400

    Add print commands to search through the nested data structure

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,37 +1,40 @@
 #Info is from the Kanto Pokedex...obvs.
 
 pokemon = {
-	grass: {
+	grass_type: {
+		for_battle: "Not particularly",
 		grass_stats: {
-			only_grass: 0;
+			only_grass: 0,
 			partial_grass: 14
-		} 
+		}, 
 
-		water_examples: [
+		grass_examples: [
 		"Venusaur",
 		"Vileplume",
 		"Exeggutor"
 		]
-	}
+	},
 
-	psychic: {
+	psychic_type: {
+		for_battle: "I like having one for two on my team",
 		psychic_stats: {
 			only_psychic: 8,
 			partial_psychic: 6
-		} 
+		}, 
 
-		water_examples: [
+		psychic_examples: [
 		"Mr.Mime",
 		"Alakazam",
 		"Hypno"
 		]
-	}
+	},
 
-	water: {
+	water_type: {
+		for_battle: "Sometimes like against Rock or Ground-types",
 		water_stats: {
 			only_water: 18,
 			partial_water: 14
-		} 
+		}, 
 
 		water_examples: [
 		"Blastoise",
@@ -40,3 +43,12 @@ pokemon = {
 		]
 	}
 }
+
+p pokemon[:water_type][:for_battle]
+p pokemon[:water_type][:water_stats][:partial_water]
+p pokemon[:psychic_type][:psychic_examples][2] = "Mewtwo"
+p pokemon[:grass_type][:for_battle] = "Who am I kidding? I never use Grass-types"
+p pokemon[:psychic_type][:psychic_examples].push pokemon[:grass_type][:grass_examples].delete("Exeggutor")
+
+
+p pokemon
