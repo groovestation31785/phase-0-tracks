c7de692ef5e6ddf1820d44cf1715d8c4a5474ed3
Allyse Groover <angroover@gmail.com>
2016-11-03 16:04:27 -0400

    Pseudocode the bubble sort

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -11,7 +11,7 @@ def search_array(arr, digit)
 	end	
 end
 
-p search_array(arr, 1212345)
+p search_array(arr, 145)
 
 #Release 1
 def FibSeq(n)
@@ -28,3 +28,19 @@ def FibSeq(n)
 	end
 	return array[0..n]
 end
+
+
+print FibSeq(10)
+
+#Release 2
+#Implement the bubble sort method
+#For an array of numbers, order them smallest to largest.
+#Look at the first two numbers and determine which one is smallest and which is largest
+#If they are already in ascending order, do not swap, otherwise, switch the number's positions
+#Look at the number in the second and third positions however you want. 
+#Do the same for the second and third numbers as you did the first and second.
+#If the first number and the new second number are not in ascending order, switch the two numbers.
+#Otherwise, continue to the third and fourth number
+#Continue making these comparisons until all of the numbers have been sorted into ascending order.
+
+
