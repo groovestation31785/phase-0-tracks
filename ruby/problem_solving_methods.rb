630833b4058270394904cb4af9bce7a69a77f933
Allyse Groover <angroover@gmail.com>
2016-11-03 16:02:52 -0400

    Add Fibonacci method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,3 +1,4 @@
+#Release 0
 arr = [42, 89, 23, 1, 2432, 12, 67, 30]
 def search_array(arr, digit)
 	index = 0
@@ -11,3 +12,19 @@ def search_array(arr, digit)
 end
 
 p search_array(arr, 1212345)
+
+#Release 1
+def FibSeq(n)
+
+	array = [0]
+	n.times do |i|
+		if i == 0
+			array[i] = 0
+		elsif i == 1
+			array[i] = 1
+		else 
+			array[i] = array[i-1] + array[i-2]
+		end
+	end
+	return array[0..n]
+end
