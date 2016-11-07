c40f166c196da40ed70e1f6a2dbda658a13470be
Allyse Groover <angroover@gmail.com>
2016-11-07 15:40:11 -0500

    Add class design for puppies

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+=begin
+	SPECIES
+		- Canis familiaris
+	
+	CHARACTERISTICS
+		- Breed: varies
+		- Height: varies
+		- Weight: varies
+		- Legs: 4
+		- Eyes: 2
+		- Ears: 2
+		- Color: varies
+
+	BEHAVIOR
+		- Fetch
+		- Bark
+		- Love
+		- Sleep
+		- Follow commands
+=end
