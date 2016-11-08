8d4615baa91ce75c9e8a9b868ec30e9cffc35887
Allyse Groover <angroover@gmail.com>
2016-11-08 15:53:11 -0500

    Add getter and setter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,10 @@
 class Santa
 
-	def initialize
+
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
 	end
 
 	def speak
@@ -11,9 +14,41 @@ class Santa
 	def eat_milk_and_cookies(type)
 		puts "That was a good #{type} cookie!"
 	end
+
+	def celebrate_birthday(age)
+		age += 1
+		puts "Santa just turned #{age}!"
+	end
+
+	def get_mad_at(reindeer)
+		reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		
+		reindeer_ranking.delete_if { |culprit| culprit == reindeer}
+		new_reindeer_ranking = reindeer_ranking.push(reindeer)
+	end
+
+	def age 
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+
 end
 
-nikolas = Santa.new
+nikolas = Santa.new("sissy-boy", "Caucasian")
+#nikolas.speak
+#nikolas.eat_milk_and_cookies("chocolate chip") 
 
-puts nikolas.speak
-puts nikolas.eat_milk_and_cookies("chocolate chip")
+#kris_kringle = Santa.new("male", "Scandanavian")
+#claus = Santa.new("transgender", "German")
+
+p nikolas.celebrate_birthday(1746)
+p nikolas.get_mad_at("Prancer")
+
+
