dc29293b37cfd75cce5bedf0aff4b672aee216f9
Allyse Groover <angroover@gmail.com>
2016-11-09 14:12:26 -0500

    Create many Santas for SantaCon in release 4

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -6,6 +6,7 @@ class Santa
 		puts "Initializing Santa instance..."
 		@gender = gender
 		@ethnicity = ethnicity
+		@age = rand(1..140)
 	end
 
 	def speak
@@ -16,8 +17,8 @@ class Santa
 		puts "That was a good #{type} cookie!"
 	end
 
-	def celebrate_birthday(age)
-		age += 1
+	def celebrate_birthday
+		@age += 1
 		puts "Santa just turned #{age}!"
 	end
 
@@ -31,19 +32,35 @@ class Santa
 
 end
 
-nikolas = Santa.new("sissy-boy", "Caucasian")
-nikolas.speak
-nikolas.eat_milk_and_cookies("chocolate chip") 
+# DRIVER CODE
+# nikolas = Santa.new("sissy-boy", "Caucasian")
+# nikolas.speak
+# nikolas.eat_milk_and_cookies("chocolate chip") 
 
-kris_kringle = Santa.new("muscle-man", "Scandanavian")
-claus = Santa.new("transgender", "feline-identified")
+# kris_kringle = Santa.new("muscle-man", "Scandanavian")
+# claus = Santa.new("transgender", "feline-identified")
 
-nikolas.celebrate_birthday(1746)
-p nikolas.get_mad_at("Dasher")
+# nikolas.celebrate_birthday
+# p nikolas.get_mad_at("Dasher")
 
-puts "I am ol' Kris Kringle. I'm a #{kris_kringle.ethnicity}."
-kris_kringle.gender = "beefcake"
+# puts "I am ol' Kris Kringle. I'm a #{kris_kringle.ethnicity}."
+# kris_kringle.gender = "beefcake"
+
+# puts "I've gain a lot of mass so I can identify as a #{kris_kringle.gender}."
 
 
-puts "I've gain a lot of mass so I can identify as a #{kris_kringle.gender}."
+list_santas = []
 
+possible_genders = ["male", "female", "agender", "bigender", "depends on the time of year", "None of these", "transgender", "rather not disclose that information"]
+possible_ethnicities = ["Himalayan Yeti", "white", "Korean", "Latino", "Scandanavian", "Malaysian Marsian", "Super multiethinic", "Nunyabiz"]
+
+
+100.times do
+	possible_genders.length.times do |s|
+		list_santas << Santa.new(possible_genders.sample, possible_ethnicities.sample)
+	end
+end
+
+list_santas.each do |i|
+	puts "This is a #{i.ethnicity} #{i.gender}"
+end
