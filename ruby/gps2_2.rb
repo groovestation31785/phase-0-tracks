1e40237625e2ac8d53b3ab541afb0ef8f6a1d4a2
Allyse Groover <angroover@gmail.com>
2016-11-08 21:29:06 -0500

    Pseudocode the methods for the list

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,42 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # Start with a blank list
+  	# Take the item given to us 
+  	# Split into separate multiple items
+  	# Use the items given to put into our blank list
+  # set default quantity to one each
+  # print the list to the console [can you use one of your other methods here?]
+# output: array
+
+# Method to add an item to a list
+# input: item name
+# steps: 
+	# Take item that needs added
+	# Add item to the end of the list
+# output: array
+
+# Method to remove an item from the list
+# input: item that we don't want on our list
+# steps:
+	# Identity the item that needs to go
+	# Find the item within our list
+	# Remove said item
+# output: array 
+
+# Method to update the quantity of an item
+# input: the item name and the amount of that item
+# steps:
+	# Look through the list to find the item
+	# Match the item with the corresponding amount
+	# Set the item/quantity to the updated list
+	# Do this for all the items to create a item/quantity pair
+# output: hash
+
+# Method to print a list and make it look pretty
+# input: none
+# steps:
+	# Create a template for our list information
+	# Cycle through the list
+	# Print the list using the template
+# output: strings
