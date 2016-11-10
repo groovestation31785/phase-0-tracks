dfa72e5760bc9cb50183ea3d3c7ab6896b314ca8
Allyse Groover <angroover@gmail.com>
2016-11-09 20:53:16 -0500

    Create module/class mixin for shout

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,15 +1,35 @@
+#module Shout
+#	def self.yell_angrily(words)
+#		words + "!!!" + " :("
+#	end
 
-module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
-	end
-
-	def self.yelling_happily(words)
-		words + "!!!" + " :D"
-	end
-end
+#	def self.yelling_happily(words)
+#		words + "!!!" + " :D"
+#	end
+#end
 
 # p Shout.yell_angrily("I don't understand")
 # p Shout.yelling_happily("I understand")
+
+
+module Shout
+	def illicit_hollering(words)
+		"#{words}! That makes me wanna shout!!"
+	end
+
+	def good_news(words)
+		puts words
+		puts "Oh my gosh!! I'm so happy for you!!"
+	end
+end
+
+class Schoolboys
+	include Shout
+end
+
+class Women
+	include Shout
+end
+
 
 
