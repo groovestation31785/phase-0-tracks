f5cbf0cfc6c16a48c484a87f8519984cbbceb1b2
Allyse Groover <angroover@gmail.com>
2016-11-16 12:52:06 -0500

    Refactor predicted deaths method

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -15,6 +15,7 @@ class VirusPredictor
   # This is ran every time a new instance of the class in created
   # There are three arguments when created: state, PD, population
   # Those three attributes are set as instance variables to the arguments
+
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
@@ -43,28 +44,32 @@ class VirusPredictor
 
   def predicted_deaths #(population_density, population, state)
     # predicted deaths is solely based on population density
+
     number_of_deaths = 0
     nums_check = {200 => 0.4, 150 => 0.3, 100 => 0.2, 50 => 0.1}
     nums_check.each do |key, value| 
       if @population_density >= key
         number_of_deaths = (@population * value).floor
+        break
       else
         number_of_deaths = (@population * 0.05).floor
       end
     end
 
-    # if @population_density >= 200
-    #   number_of_deaths = (@population * 0.4).floor
-    # elsif @population_density >= 150
-    #   number_of_deaths = (@population * 0.3).floor
-    # elsif @population_density >= 100
-    #   number_of_deaths = (@population * 0.2).floor
-    # elsif @population_density >= 50
-    #   number_of_deaths = (@population * 0.1).floor
-    # else
-    #   number_of_deaths = (@population * 0.05).floor
-    # end
 
+=begin
+     if @population_density >= 200
+       number_of_deaths = (@population * 0.4).floor
+     elsif @population_density >= 150
+       number_of_deaths = (@population * 0.3).floor
+     elsif @population_density >= 100
+       number_of_deaths = (@population * 0.2).floor
+     elsif @population_density >= 50
+       number_of_deaths = (@population * 0.1).floor
+     else
+       number_of_deaths = (@population * 0.05).floor
+     end
+=end
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
@@ -79,16 +84,20 @@ class VirusPredictor
   def speed_of_spread #(population_density, state) in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
+   
     nums_check = {200 => 0.5, 150 => 1, 100 => 1.5, 50 => 2}
+    speed = 0.0
     nums_check.each do |key, value|
+      
       if @population_density >= key
         speed += value
-       elsif @population_density < 50
+        break
+       else
          speed += 2.5
       end
     end
-    
+
+
 =begin
     speed = 0.0
 
@@ -122,18 +131,22 @@ STATE_DATA.each do |key, value|
   state_name.virus_effects
 end
 
-#alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-#alabama.virus_effects
+=begin
+alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+alabama.virus_effects
 
-#jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-#jersey.virus_effects
+jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
+jersey.virus_effects
 
-#california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-#california.virus_effects
+california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
+california.virus_effects
 
-#alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-#alaska.virus_effects
+alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
+alaska.virus_effects
 
+idaho = VirusPredictor.new("Idaho", STATE_DATA["Idaho"][:population_density], STATE_DATA["Idaho"][:population])
+idaho.virus_effects
+=end
 
 #=======================================================================
 # Reflection Section
