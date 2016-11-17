2cb86c58a9f9a6735eef99039a6f928ae672badc
Allyse Groover <angroover@gmail.com>
2016-11-17 16:03:00 -0500

    Pseudocode a function for finding the longest string in an array

---
+++ js/algos.js
@@ -0,0 +1,11 @@
+// FIND THE LONGEST PHRASE
+// Create a program that takes a list of words/phrases
+// Cycle throught the list
+//	- Do this as many times as there is items on the list
+// Determine the length of each individual string
+// Store the lengths of each items to use for comparison
+// If one item's length is greater than another's:
+//	- The former item is longer
+// Continue comparing each item to the one that has the longest length
+// At the end, return the item with the longest length
+
