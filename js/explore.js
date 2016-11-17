098ceb67d9598dcf8a0738ff15e43138f9f49210
Allyse Groover <angroover@gmail.com>
2016-11-17 14:10:28 -0500

    Pseudocode process of reversing a string

---
+++ js/explore.js
@@ -0,0 +1,5 @@
+// PSEUDOCODE FOR JAVASCRIPT PROGRAM
+// Create a program that receives a word or sentence
+// Take said word or sentence and determine its length
+// Cycle through the word/sentence starting with the last character
+// Combine all the letters to create a single word/statement but in reverse
