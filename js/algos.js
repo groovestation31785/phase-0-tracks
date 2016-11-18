9a674eb16c671f2f516e28ea497319f1bdf12f90
Allyse Groover <angroover@gmail.com>
2016-11-18 10:02:09 -0500

    Pseudocode the key/value comparison function

--- js/algos.js
+++ js/algos.js
@@ -1,7 +1,7 @@
 // FIND THE LONGEST PHRASE
 // Create a program that takes a list of words/phrases
 // Cycle throught the list
-//	- Do this as many times as there is items on the list
+//	- Do this as many times as there are items on the list
 // Determine the length of each individual string
 // Store the lengths of each items to use for comparison
 // If one item's length is greater than another's:
@@ -13,7 +13,7 @@ function longestPhrase(arr) {
   var leng = 0;
   
   for(var i = 0; i < arr.length; i++ ) {
-    if(arr[i].length > leng) {
+    if (arr[i].length > leng) {
         var leng = arr[i].length;
         var longest = arr[i];
     }      
@@ -22,3 +22,15 @@ function longestPhrase(arr) {
 }
 
 console.log(longestPhrase(["elephant", "giraffe", "brontosaurs", "boa constrictor", "A Yeti so huge that you would probably die from the mere size of it."]));
+
+// FIND A KEY-VALUE MATCH
+// Create a program starts with two sets of data
+// Cycle through the list
+//	- Do this as many times as there are items in each set
+//	- During each cycle:
+//		- The first pair should be compared to all the other pairs in the other data set
+//		- If the two sets of data have a data pair that matches:
+//			- Then return true
+//		- Otherwise:
+//			- Return false (meaning none of the pairs match)
+// Repeat the comparison process as needed to compare all the possible 
