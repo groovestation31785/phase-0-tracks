844ed22a3c8e4d7427cf8e2e0d093d925e243f26
Allyse Groover <angroover@gmail.com>
2016-11-19 16:33:21 -0500

    Create loop for word generator function

--- js/algos.js
+++ js/algos.js
@@ -1,4 +1,4 @@
-// FIND THE LONGEST PHRASE
+// RELEASE 0: FIND THE LONGEST PHRASE
 // Create a program that takes a list of words/phrases
 // Cycle throught the list
 //	- Do this as many times as there are items on the list
@@ -26,8 +26,7 @@ function longestPhrase(arr) {
 // console.log(longestPhrase(["Captain Crunch", "Lucky Charms", "Trix", "Cheerios"]));
 
 
-
-// FIND A KEY-VALUE MATCH
+// RELEASE 1: FIND A KEY-VALUE MATCH
 // Create a program starts with two sets of data
 // Cycle through the list
 //	- Do this as many times as there are items in each set
@@ -43,7 +42,6 @@ function compareObjects(oneObject, twoObject) {
   var oneValueArray = Object.keys(oneObject);
   var twoValueArray = Object.keys(twoObject);
   
-    
   for (var i = 0; i < oneValueArray.length; i++) {
     var prop = oneValueArray[i];
     
@@ -55,8 +53,45 @@ function compareObjects(oneObject, twoObject) {
 }
 	
 
-var jimData = {name: "Jim Halpert", employer: "Dunder Mifflin", occupation: "paper salesman"};
-var kevinData = {name: "Kevin Malone", employer: "Dunder Mifflin", occupation: "accountant"};
+var jimData = {name: "Jim Halpert", dmBranch: "Scranton", occupation: "paper salesman"};
+var kevinData = {name: "Kevin Malone", dmBranch: "Scranton", occupation: "accountant"};
+var andyData = {name: "Andy Bernard", dmbranch: "Stamford", occupation: "paper salesman"}
 
 
-console.log(compareObjects(jimData, kevinData));
+// console.log(compareObjects(jimData, kevinData));
+// console.log(compareObjects(kevinData, andyData));
+// console.log(compareObjects(jimData, andyData));
+
+
+// RELEASE 2: GENERATE RANDOM TEST DATA
+// Create a program that accepts a number to represent a generated list with that many items
+// Provide a list of letters that can be accessed to generate words
+// Cycle through the alphabet so that it chooses a random letter
+// - The cycle will loop the same number of times that
+// Create a new variable that can have letters added to it with each cycle
+//	- The words must be between 1 and 10 letters long
+
+function wordGenerator(arrayLength) {
+    var alphabet = "abcdefghijklmnopqrstuvwxyz";
+    var wordArray = [];
+
+    for (var i = 0; i < arrayLength; i++) {
+    	var randomNum = Math.round((Math.random() * 10) + 1);
+    	var nonsenseWord = "";
+    	for (var n = 0; n < randomNum; n++) {
+    		nonsenseWord += alphabet[i + Math.round((Math.random() * 10) + 1 )];
+   		}
+    wordArray.push(nonsenseWord);
+	}
+  return wordArray;
+}
+
+
+// console.log(wordGenerator(3));
+// console.log(wordGenerator(8));
+
+for (var i = 0; i < 10; i++) {
+	var finalArray = wordGenerator(6);
+	console.log(finalArray)
+	console.log(longestPhrase(finalArray));
+}
