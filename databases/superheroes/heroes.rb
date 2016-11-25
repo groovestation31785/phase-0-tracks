8ce536d1012ede36dc9d4853c4fe621cbcd3cb68
Allyse Groover <angroover@gmail.com>
2016-11-25 16:42:31 -0500

    Create driver code for Ruby methods

--- databases/superheroes/heroes.rb
+++ databases/superheroes/heroes.rb
@@ -20,12 +20,42 @@ SQL
 # Create the table
 db.execute(heroes_table_cmd)
 
-# Create a new tech hero. This adds a name, age, specialty, and declare that no one has yet won a Nobel Prize
+# Create a new tech hero. This adds a name, age, and specialty
 def create_new_hero(db, name, age, specialty)
   db.execute("INSERT INTO tech_heroes(name, age, specialty) VALUES (?, ?, ?)", [name, age, specialty])
 end
 
-
+# Create multiple entries for the list of Silicon Valley's finest
 # 10.times do
 #   create_new_hero(db, Faker::Name.name, Faker::Number.number(2), Faker::Company.bs)
 # end
+
+
+# This helps us find all the awesome tech heroes by name
+def game_changers(db)
+	fav_hero = db.execute("SELECT name, specialty FROM tech_heroes WHERE name LIKE '") 
+	fav_hero.each do |person|
+		puts "#{person['name']} is working to #{person['specialty']}!"
+	end
+end
+
+
+
+# Some of our heroes are no keeping up with the newer technologies
+# We are going to kindly suggest that they retire
+def retirement(db)
+	retirees = db.execute("UPDATE tech_heroes SET specialty="Enjoying retirement" WHERE age > 70")
+	retirees.each do |name|
+		puts "Thank you for all your years of work, #{name['name']}!!!"
+	end
+end
+
+# DRIVER CODE
+find_hero_name(db)
+retirement(db)
+
+
+
+# There are three things I would have liked to do, but I could not figure out how to make it work
+#	- I wanted to add a Nobel Prize column. Everyone would start with "false" , but later I would give it to the people with specific specialties. For example if a specialty had the prefix "trans-" in the description, "false" would be changed to "true"
+#	- Instead of hard-coding some of the search parameters, how would 
