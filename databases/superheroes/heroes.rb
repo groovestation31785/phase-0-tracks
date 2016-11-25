7d508ba22480d27161390164554b4ec060008974
Allyse Groover <angroover@gmail.com>
2016-11-25 15:24:26 -0500

    Update methods to fill table with data

--- databases/superheroes/heroes.rb
+++ databases/superheroes/heroes.rb
@@ -13,8 +13,7 @@ heroes_table_cmd = <<-SQL
 		id INTEGER PRIMARY KEY, 
 		name VARCHAR(255),
 		age INTEGER,
-		specialty VARCHAR(255),
-		nobel_prize BOOLEAN
+		specialty VARCHAR(255)
 		);
 SQL
 
@@ -23,13 +22,10 @@ db.execute(heroes_table_cmd)
 
 # Create a new tech hero. This adds a name, age, specialty, and declare that no one has yet won a Nobel Prize
 def create_new_hero(db, name, age, specialty)
-  db.execute("INSERT INTO tech_heroes(name, age, specialty) VALUES (?, ?, ?, "false")", [name, age, specialty])
+  db.execute("INSERT INTO tech_heroes(name, age, specialty) VALUES (?, ?, ?)", [name, age, specialty])
 end
 
-def give_nobel_prize(db, )
-	db.execute("INSERT INTO tech_heroes(nobel_prize) VALUES ("true") WHERE age > 40", [nobel_prize]) 
-end
 
-10.times do
-  create_new_hero(db, Faker::Name.name, Faker::Number.digit, Faker::Company.bs)
-end
+# 10.times do
+#   create_new_hero(db, Faker::Name.name, Faker::Number.number(2), Faker::Company.bs)
+# end
